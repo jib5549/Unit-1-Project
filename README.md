0b52c927b95cdb3919fa171efdab3bafcd993b05
jib5549 <jib5549@gmail.com>
2023-05-02 22:47:15 -0400

    added renderMessage and working on renderControls

--- README.md
+++ README.md
@@ -1,14 +1,25 @@
-# Tic-Tac-Toe GAME
+# Tic-Tac-Toe DOM GAME
 ## OBJECTIVE:
-## GAME DESCRIPTION
+    This project for GA SEI aims to develop a Tic Tac Toe game using HTML, CSS, and JS. It mainly consists of developing and implementing a computer program that plays Tic Tac Toe against another player. It must be best three out of five.
+## GAME DESCRIPTION:
+    Tic Tac Toe  is a classic two-player strategy game played on a 3x3 grid. The players take turns marking either an X or an O on the grid with the goal of getting three of their marks in a row, either horizontally, vertically, or diagonally, before their opponent does.
 ## GAME RULES:
-<p>Tic Tac Toe  is a classic two-player strategy game played on a 3x3 grid. The players take turns marking either an X or an O on the grid with the goal of getting three of their marks in a row, either horizontally, vertically, or diagonally, before their opponent does.</p>
+    you know the rules ;)
 
-***hi***
-- HTML
-- CSS
-- JS
-- GIT
+
+
+
+
+
+
+
+
+
+## TECHNOLOGIES USED:
+    -HTML
+    -CSS
+    -JS
+    -GIT
 
 
 
--- app.js
+++ app.js
@@ -1,39 +1,41 @@
-/*----- constants -----*/
+/*----- ----------------------------------------- constants ---------------------------------------------- */
 const PLAYERS = {
-    player1 : 'jim', // set player jim to 0
-    player2 : 'dwight' // set player dwight to 0
+    '1' : 'X', // set player to 1
+    '-1' : 'O' // set player to -1
 };
+const winConditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+]
+const box = document.querySelectorAll('.box')
 
-/*----- state variables -----*/
+/*----- ----------------------------------------- state variables ------------------------------------*/
 let turn; //this will be player1 and player2
 let board; //this will the board for the game tic tac toe
 let winner; // this will set to null and eventually a winner, loser, or a tie will come out
 
-/*----- cached elements  -----*/
-const startBtn = document.querySelector(#startBtn); // 
-const restartBtn = document.querySelector(#restartBtn); //
-const musicBtn = document.querySelector(#musicBtn); //
 
-/*----- event listeners -----*/
+/*----- ----------------------------------------- cached elements  ------------------------------------*/
+const messageEl = document.querySelector('h1');
+const restartBtn = document.getElementById('restartBtn')
 
-startBtn.addEventListener('click', init()) //
-restartBtn.addEventListener('click', ()) // this 
-musicBtn.addEventListener('click', ()) // this button will allow the player to have the music on or off, it needs a function
+/*----- ----------------------------------------- event listeners ------------------------------------*/
+
+// restartBtn.addEventListener('click', ()) // this 
+// musicBtn.addEventListener('click', ()) // this button will allow the player to have the music on or off, it needs a function
 
 
-/*----- functions -----*/
+/*----- ----------------------------------------- functions ---------------------------------------------- */
 init();
 
-getWinner() { // 
-
-}
-
-checkWinner() { // 
-
-}
-
-function init() { // this resets the board with clean sheet
-    turn = player1;
+function init() { // this resets the board with a clean sheet
+    turn = 1;
     board = [
     [0, 0, 0,], // col 0 represents the tic tac toe board in 2d 
     [0, 0, 0,], // col 1 represents the tic tac toe board in 2d 
@@ -42,4 +44,37 @@ function init() { // this resets the board with clean sheet
 ];
 winner = null; // since it's the beginning there is no winner or loser
 render(); 
+}
+
+// this function transfers the state of our application to the DOM
+function render () {
+    renderBoard();
+    renderMessage();
+    renderControls();
+}
+
+function renderBoard() {
+    board.forEach(function(colArr, colIdx) {
+        colArr.forEach(function(rowVal, rowIdx) {
+            
+        })
+    })
+}
+
+function renderMessage() {
+    if(winner === 'T') {
+        messageEl.innerText = "It's a Tie!!"
+    } else if(winner) {
+        messageEl.innerText = `${PLAYERS[winner]}'s Wins!!`
+    } else {
+        messageEl.innerText = `${PLAYERS[turn]}'s turn`
+    }
+}
+
+function renderControls() {
+    if(!winner) {
+        restartBtn.style.visibility = 'hidden'
+    } else  {
+        restartBtn.style.visibility = 'visible'
+    }
 }
