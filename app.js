5a8354ca7d0da67d7b75a0a09b8b08602222e3bd
jib5549 <jib5549@gmail.com>
2023-05-06 17:50:08 -0400

    still trying on win conditon loop+logic

--- app.js
+++ app.js
@@ -1,117 +1,124 @@
 /*----- ----------------------------------------- constants ---------------------------------------------- */
 const PLAYERS = {
-    '1' : 'X', // set player to 1
-    '-1' : 'O', // set player to -1
+  1: "X", // set player to 1
+  "-1": "O", // set player to -1
+  null: "", //empty string
 };
 
-// const winConditions = [
-//     [0, 1, 2],
-//     [3, 4, 5],
-//     [6, 7, 8],
-//     [0, 3, 6],
-//     [1, 4, 7],
-//     [2, 5, 8],
-//     [0, 4, 8],
-//     [2, 4, 6]
-// ]
-
-
+const winConditions = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
 
 /*----- ----------------------------------------- state variables ------------------------------------*/
 let turn; //this will be player1 and player2
-let board; //this will the board for the game tic tac toe
 let winner; // this will set to null and eventually a winner, loser, or a tie will come out
-let currentPlayer = 'X';
-
-
+let currentPlayer = "X";
+let score = {
+  // setting a new variable "score" to keep track of the scores for both players
+  '1' : 0,
+  '-1' : 0,
+};
+let board;
 
 /*----- ----------------------------------------- cached elements  ------------------------------------*/
-const messageEl = document.getElementById('message');
-const restartBtn = document.getElementById('restartBtn')
-const boxes = [...document.querySelectorAll('.box')]
-const audio = document.getElementById('audio')
+const messageEl = document.getElementById("message");
+const restartBtn = document.getElementById("restartBtn");
+const audio = document.getElementById("audio");
+const gameBoard = document.getElementById("board");
 
 /*----- ----------------------------------------- event listeners ------------------------------------*/
 
-restartBtn.addEventListener('click', init)
-
-boxes.forEach((box) => {
-    box.addEventListener('click', boxClicked) 
-}) 
-
-
-
+restartBtn.addEventListener("click", init);
 
 /*----- ----------------------------------------- functions ---------------------------------------------- */
 init();
 
-function init() { // this resets the board with a clean sheet
-    turn = 1
-    board = [
-    [0, 0, 0,], // col 0 represents the tic tac toe board in 2d 
-    [0, 0, 0,], // col 1 represents the tic tac toe board in 2d 
-    [0, 0, 0,], // col 2 represents the tic tac toe board in 2d 
-//  r0 r1  r2
-];
-winner = null; // since it's the beginning there is no winner or loser
-render(); 
+function init() {
+  // this resets the board with a clean sheet
+  turn = 1;
+  winner = null; // since it's the beginning there is no winner or loser
+  board = [];
+  createBoard();
+  render();
+}
+
+function createBoard() {
+  gameBoard.innerHTML = "";
+  for (let i = 0; i < 9; i++) {
+    const box = document.createElement("div");
+    box.className = "box";
+    box.id = i;
+    box.addEventListener("click", boxClicked);
+    gameBoard.appendChild(box);
+  }
+}
+
+function checkWinner() {
+    console.log(board);
+  for (let i = 0; i < winConditions.length; i++) {
+    if (
+      Math.abs(
+        board[winConditions[i][0]] +
+          board[winConditions[i][1]] +
+          board[winConditions[i][2]] ===
+          3
+      )
+    ) {
+        console.log('winner');
+      return board[winConditions[i][0]];
+    }
+  }
+  if (board.includes(null)) return false;
+  return "T";
 }
 
 // this function transfers the state of our application to the DOM
-function render () {
-    renderBoard();
-    turnMessage();
-    renderControls();
-}
-
-function renderBoard() {
-    board.forEach(function(colArr, colIdx) {
-        colArr.forEach(function(rowVal, rowIdx) {
-            const cellId = `c${colIdx}r${rowIdx}`
-            const cellEl = document.getElementById(cellId)
-            console.log(cellEl);
-        })
-    })
+function render() {
+  turnMessage();
+  renderControls();
 }
 
 function turnMessage() {
-    if(winner === 'T') {
-        messageEl.innerText = "It's a Tie!!"
-    } else if(winner) {
-        messageEl.innerText = `${PLAYERS[winner]}'s Wins!!`
-    } else {
-        messageEl.innerText = `${PLAYERS[turn]}'s turn`
-    }
-}
-
-function renderControls() {
-    if(!winner) {
-        restartBtn.style.visibility = 'hidden'
-    } else  {
-        restartBtn.style.visibility = 'visible'
-    }
+  if (winner === "T") {
+    messageEl.innerText = "It's a Tie!!";
+  } else if (winner) {
+    messageEl.innerText = `${PLAYERS[winner]}'s Wins!!`;
+  } else {
+    messageEl.innerText = `${PLAYERS[turn]}'s turn`;
+  }
 }
 
 function boxClicked(event) {
-   if(event.target.textContent !== '') {
-    return // prevent overwriting existing moves
-   }
-   event.target.textContent = currentPlayer;
+  if (event.target.textContent !== "") {
+    return; // prevent overwriting existing moves
+  }
+  event.target.textContent = currentPlayer;
 
-   currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+  currentPlayer = currentPlayer === "X" ? "O" : "X";
 
-   messageEl.textContent = `${currentPlayer}'s turn`
+  messageEl.textContent = `${currentPlayer}'s turn`;
+  checkWinner()
 }
 
-
-function getWinner() {
-
+function renderControls() {
+  if (!winner) {
+    restartBtn.style.visibility = "hidden";
+  } else {
+    restartBtn.style.visibility = "visible";
+  }
 }
 
 function playAudio() {
-    if(audio.paused) {
-        audio.play()
-    } else {
-        audio.pause()
-    }
+  if (audio.paused) {
+    audio.play();
+  } else {
+    audio.pause();
+  }
 }
